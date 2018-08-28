a6e7bb011e7369e5018e2ac18041fd099f1bd327
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-08-28 11:13:54 -0400

    Update to include finished lecture code

--- 01_apis_and_iteration/app.rb
+++ 01_apis_and_iteration/app.rb
@@ -1,2 +1,46 @@
 # Google Books API "https://www.googleapis.com/books/v1/volumes?q={#{term}}"
 
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
+def make_request
+  beef = RestClient.get "https://www.reddit.com/.json"
+  JSON.parse(beef.body)
+  # binding.pry
+end
+
+def view(json)
+
+  post_array = json["data"]["children"].map do |post|
+    {
+      title: post["data"]["title"],
+      subreddit: post["data"]["subreddit"],
+      score: post["data"]["score"]
+    }
+  end
+
+  # binding.pry
+
+  post_array.each do |post|
+    puts "====================="
+    puts "Title: #{post[:title]}"
+    puts "Subreddit: #{post[:subreddit]}"
+    puts "Score: #{post[:score]}"
+    puts "====================="
+  end
+
+
+  # json["data"]["children"].each do |post|
+  #   puts "====================="
+  #   puts "Title: #{post["data"]["title"]}"
+  #   puts "Subreddit: #{post["data"]["subreddit"]}"
+  #   puts "Score: #{post["data"]["score"]}"
+  #   puts "====================="
+  # end
+  # binding.pry
+  # 0
+end
+
+view(make_request)
