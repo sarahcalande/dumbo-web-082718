4459d96f1c2f98224fa38c131c241116e631d6fc
matt-mcalister <matt.mcalister93@gmail.com>
2018-09-20 10:58:17 -0400

    ar review initial lecture code

---
+++ 13-active-record-review/db/seeds.rb
@@ -0,0 +1,18 @@
+
+cults = [  {
+    name: "Heaven's Gate",
+    location: "CA"
+  },
+  {
+    name: "Jonestown",
+    location: "GY"
+  },
+  {
+    name: "Rajnesshees",
+    location: "ID"
+  },
+  {
+    name: "Branch Dividians",
+    location: "TX"
+  }
+]
